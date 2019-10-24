693be0b4b0dfd731088a00ad0316d6ddc810877b
I-PreethamRoy <dasari.preetham@i-exceed.com>
2019-10-24 14:16:52 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import {BodyComponent} from'./scripts/body/body.component';
-import {FormComponent} from'./scripts/form/form.component';
-
-import {GridcolComponent} from'./scripts/gridcol/gridcol.component';
-import {GridrowComponent} from'./scripts/gridrow/gridrow.component';
-import {InputComponent} from'./scripts/input/input.component';
-import {PanelComponent} from'./scripts/panel/panel.component';
-import {PanelsecComponent} from'./scripts/panelsec/panelsec.component';
-import {ScreenComponent} from'./scripts/screen/screen.component';
-import {SeccolComponent} from'./scripts/seccol/seccol.component';
-import {SecrowComponent} from'./scripts/secrow/secrow.component';
-
-
-import { AppComponent } from './app.component';
+import {bodycomponent} from'./scripts/body/body.component';
+import {formcomponent} from'./scripts/form/form.component';
+import {gridcolcomponent} from'./scripts/gridcol/gridcol.component';
+import {gridrowcomponent} from'./scripts/gridrow/gridrow.component';
+import {inputcomponent} from'./scripts/input/input.component';
+import {panelcomponent} from'./scripts/panel/panel.component';
+import {panelseccomponent} from'./scripts/panelsec/panelsec.component';
+import {screencomponent} from'./scripts/screen/screen.component';
+import {seccolcomponent} from'./scripts/seccol/seccol.component';
+import {secrowcomponent} from'./scripts/secrow/secrow.component';
+import {AppComponent } from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
