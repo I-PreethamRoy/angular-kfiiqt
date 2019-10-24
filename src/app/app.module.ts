9e7f96b852a298d8c6f9f59c10667f4ca52a0784
I-PreethamRoy <dasari.preetham@i-exceed.com>
2019-10-24 14:08:51 +0530

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {BodyComponent} from'./scripts/body/body.component';
+import {FormComponent} from'./scripts/form/form.component';
+
+import {GridcolComponent} from'./scripts/gridcol/gridcol.component';
+import {GridrowComponent} from'./scripts/gridrow/gridrow.component';
+import {InputComponent} from'./scripts/input/input.component';
+import {PanelComponent} from'./scripts/panel/panel.component';
+import {PanelsecComponent} from'./scripts/panelsec/panelsec.component';
+import {ScreenComponent} from'./scripts/screen/screen.component';
+import {SeccolComponent} from'./scripts/seccol/seccol.component';
+import {SecrowComponent} from'./scripts/secrow/secrow.component';
+
 
 import { AppComponent } from './app.component';
 
